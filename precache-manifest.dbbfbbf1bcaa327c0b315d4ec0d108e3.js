dc4fa48d24cf7a369689d70eb8e7c32b9e686187
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-11 13:41:13 +0100

    Updates

---
+++ precache-manifest.dbbfbbf1bcaa327c0b315d4ec0d108e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e43ce5130ab2463c4d7b2eee3d4e28",
+    "url": "/react-router-dom/index.html"
+  },
+  {
+    "revision": "c7a348672110e486abf6",
+    "url": "/react-router-dom/static/css/main.40c61ddd.chunk.css"
+  },
+  {
+    "revision": "927ecee91fdf66194f86",
+    "url": "/react-router-dom/static/js/2.1704c6ed.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-router-dom/static/js/2.1704c6ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a348672110e486abf6",
+    "url": "/react-router-dom/static/js/main.5f6f7f24.chunk.js"
+  },
+  {
+    "revision": "8836a7d564560f321fed",
+    "url": "/react-router-dom/static/js/runtime-main.1c36d53b.js"
+  }
+]);
